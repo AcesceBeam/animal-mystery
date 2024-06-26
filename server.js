b2fba5cda0c2b35b8fb1fc9a6c1d9987761125c4
AcesceBeam <107534159+AcesceBeam@users.noreply.github.com>
2024-06-26 04:31:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,77 +1,85 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname + '/public'));
-
+const clients = {};
 const rooms = {};
+
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
 
     socket.on('createRoom', () => {
-        const roomId = uuidv4();
-        rooms[roomId] = {
-            clients: {},
-            currentClueIndex: 0
-        };
-        rooms[roomId].clients[socket.id] = { score: 0 };
+        const roomId = generateRoomId();
+        rooms[roomId] = [];
         socket.join(roomId);
-        socket.emit('roomCreated', roomId);
+        clients[socket.id] = { score: 0, roomId };
+        rooms[roomId].push(socket.id);
+        io.to(socket.id).emit('roomCreated', roomId);
     });
 
     socket.on('joinRoom', (roomId) => {
         if (rooms[roomId]) {
-            rooms[roomId].clients[socket.id] = { score: 0 };
             socket.join(roomId);
-            socket.emit('roomJoined', roomId);
-            io.to(roomId).emit('updateLeaderboard', rooms[roomId].clients);
+            clients[socket.id] = { score: 0, roomId };
+            rooms[roomId].push(socket.id);
+            io.to(socket.id).emit('roomJoined', roomId);
         } else {
-            socket.emit('error', 'Room not found');
+            io.to(socket.id).emit('error', 'Room does not exist');
         }
     });
 
     socket.on('correctAnswer', (answer) => {
-        const roomId = Object.keys(socket.rooms).filter(item => item !== socket.id)[0];
-        if (rooms[roomId]) {
-            rooms[roomId].clients[socket.id].score++;
-            io.to(roomId).emit('correctAnswer', answer);
-            io.to(roomId).emit('updateLeaderboard', rooms[roomId].clients);
+        const client = clients[socket.id];
+        if (client) {
+            client.score += 1;
+            io.in(client.roomId).emit('correctAnswer', answer);
+            updateLeaderboard(client.roomId);
         }
     });
 
     socket.on('resetGame', () => {
-        const roomId = Object.keys(socket.rooms).filter(item => item !== socket.id)[0];
-        if (rooms[roomId]) {
-            rooms[roomId].currentClueIndex = 0;
-            for (let client in rooms[roomId].clients) {
-                rooms[roomId].clients[client].score = 0;
-            }
-            io.to(roomId).emit('resetGame');
-            io.to(roomId).emit('updateLeaderboard', rooms[roomId].clients);
+        const client = clients[socket.id];
+        if (client) {
+            rooms[client.roomId].forEach(clientId => {
+                clients[clientId].score = 0;
+            });
+            io.in(client.roomId).emit('resetGame');
+            updateLeaderboard(client.roomId);
         }
     });
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
-        const roomId = Object.keys(socket.rooms).filter(item => item !== socket.id)[0];
-        if (rooms[roomId]) {
-            delete rooms[roomId].clients[socket.id];
-            if (Object.keys(rooms[roomId].clients).length === 0) {
+        const client = clients[socket.id];
+        if (client) {
+            const roomId = client.roomId;
+            rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+            if (rooms[roomId].length === 0) {
                 delete rooms[roomId];
-            } else {
-                io.to(roomId).emit('updateLeaderboard', rooms[roomId].clients);
             }
+            delete clients[socket.id];
+            updateLeaderboard(roomId);
         }
+        console.log('Client disconnected');
     });
+
+    function generateRoomId() {
+        return Math.random().toString(36).substr(2, 6);
+    }
+
+    function updateLeaderboard(roomId) {
+        const leaderboard = {};
+        rooms[roomId].forEach(clientId => {
+            leaderboard[clientId] = clients[clientId];
+        });
+        io.in(roomId).emit('updateLeaderboard', leaderboard);
+    }
 });
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
