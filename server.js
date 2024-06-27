934d07405377433275e8728a4f9a56b5426d33be
AcesceBeam <107534159+AcesceBeam@users.noreply.github.com>
2024-06-27 04:20:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,74 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
-let clients = {};
+app.use(express.static(__dirname + '/public'));
+
+let rooms = {};
 
 io.on('connection', (socket) => {
-    socket.on('create-room', (roomID) => {
-        socket.join(roomID);
-        clients[socket.id] = { score: 0, room: roomID };
-        io.to(roomID).emit('update-leaderboard', clients);
+    console.log('A user connected');
+
+    socket.on('createRoom', () => {
+        let roomId = Math.random().toString(36).substring(2, 7);
+        rooms[roomId] = { players: {}, currentClue: '' };
+        socket.join(roomId);
+        socket.emit('roomCreated', roomId);
+        console.log(`Room ${roomId} created`);
     });
 
-    socket.on('join-room', (roomID) => {
-        socket.join(roomID);
-        clients[socket.id] = { score: 0, room: roomID };
-        io.to(roomID).emit('update-leaderboard', clients);
-    });
-
-    socket.on('update-score', (score) => {
-        if (clients[socket.id]) {
-            clients[socket.id].score = score;
-            io.to(clients[socket.id].room).emit('update-leaderboard', clients);
+    socket.on('joinRoom', ({ roomId, playerName }) => {
+        if (rooms[roomId]) {
+            socket.join(roomId);
+            rooms[roomId].players[playerName] = { score: 0 };
+            socket.emit('roomJoined', rooms[roomId].currentClue);
+            io.to(roomId).emit('message', `Player ${playerName} joined the room`);
+            io.to(roomId).emit('updateLeaderboard', rooms[roomId].players);
+            console.log(`Player ${playerName} joined room ${roomId}`);
+        } else {
+            socket.emit('error', 'Room does not exist');
         }
     });
 
-    socket.on('reset-game', () => {
-        if (clients[socket.id]) {
-            clients[socket.id].score = 0;
-            io.to(clients[socket.id].room).emit('update-leaderboard', clients);
+    socket.on('correctGuess', ({ playerName, score, roomId }) => {
+        if (rooms[roomId]) {
+            rooms[roomId].players[playerName].score = score;
+            io.to(roomId).emit('updateLeaderboard', rooms[roomId].players);
+        }
+    });
+
+    socket.on('newClue', ({ clue, roomId }) => {
+        if (rooms[roomId]) {
+            rooms[roomId].currentClue = clue;
+            io.to(roomId).emit('newClue', clue);
         }
     });
 
     socket.on('disconnect', () => {
-        if (clients[socket.id]) {
-            const room = clients[socket.id].room;
-            delete clients[socket.id];
-            io.to(room).emit('update-leaderboard', clients);
+        console.log('A user disconnected');
+        for (let roomId in rooms) {
+            const room = rooms[roomId];
+            for (let player in room.players) {
+                if (socket.id === player) {
+                    delete room.players[player];
+                    io.to(roomId).emit('updateLeaderboard', room.players);
+                    break;
+                }
+            }
+            if (Object.keys(room.players).length === 0) {
+                delete rooms[roomId];
+                console.log(`Room ${roomId} deleted`);
+            }
         }
     });
 });
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
